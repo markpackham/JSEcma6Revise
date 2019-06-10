5c4314f68ee1b08744b9dc8150812697c52d19c4
Mark Packham <markpackham1@gmail.com>
2019-06-10 20:10:25 +0100

    Dynamic Landing page with times learned from https://youtu.be/fSTQzlprGLI?t=596

---
+++ DynamicLandingPage/main.js
@@ -0,0 +1,7 @@
+// Dom Elements
+let time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+
+// Show Time
