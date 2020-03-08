b036167344b8b728d36ca914693245f7e70bc8b1
Mark Packham <markpackham1@gmail.com>
2020-03-08 19:06:55 +0000

    finish off animated counter

--- AnimatedCounter/main.js
+++ AnimatedCounter/main.js
@@ -1,3 +1,24 @@
 const counters = document.querySelectorAll(".counter");
-// The higher the number the faster it goes
-const speed = 200;
+// The higher the number the slower the count
+const speed = 700;
+
+counters.forEach(counter => {
+  const updateCount = () => {
+    // convert String to Int by using "+"
+    const target = +counter.getAttribute("data-target");
+    const count = +counter.innerText;
+
+    // inc is the increment
+    const inc = target / speed;
+
+    if (count < target) {
+      // use ceil to round up avoiding decimal values
+      counter.innerText = Math.ceil(count + inc);
+      setTimeout(updateCount, 1);
+    } else {
+      count.innerText = target;
+    }
+  };
+
+  updateCount();
+});
