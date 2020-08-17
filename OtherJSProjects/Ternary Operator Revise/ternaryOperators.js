c4f77d13b3041c66a8a3312ca0333d9927a5f0f7
mark <markpackham1@gmail.com>
2020-08-17 12:45:07 +0100

    add when not to use a ternary

--- OtherJSProjects/Ternary Operator Revise/ternaryOperators.js
+++ OtherJSProjects/Ternary Operator Revise/ternaryOperators.js
@@ -14,3 +14,36 @@ result =
     : "Not greater than not equal to 50";
 
 console.log(result);
+
+// WHEN TO NEVER USE A TERNARY
+// Never user ternary in this example, only use it when returning somehting or code looks confusing
+// const user = {}
+// user.valid ? user.save() : user.printErrors()
+
+// // looks much more sensible and explicit
+// if(user.valid){
+//     user.save()
+// }else{
+//     user/valid ? user.save() : user.printErrors()
+// }
+
+if (number === 0) {
+  result = "Number is 0";
+} else if (number < 10) {
+  result = "Number is less than 10";
+} else {
+  result = "Number is more than 9";
+}
+console.log(result);
+
+// Avoid nested ternaries, they can be very hard to read
+result =
+  number === 0
+    ? "Number is 0"
+    : number < 10
+    ? "Number is less than 10"
+    : "Number is more than 9";
+
+console.log(
+  result + " but you got the reuslt from an evil nested ternary, shame on you!"
+);
