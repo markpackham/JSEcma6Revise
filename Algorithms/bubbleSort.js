c6fa5bb84e38a014fa05e45015f8c3ff1f852c3b
mark <markpackham1@gmail.com>
2021-01-29 10:35:12 +0000

    learn bubble sort from, https://www.youtube.com/watch?v=IAeLoGzU4RE

---
+++ Algorithms/bubbleSort.js
@@ -0,0 +1,42 @@
+// Learn of the Bubble Sort from here, https://www.youtube.com/watch?v=IAeLoGzU4RE
+
+// This is the slow solution due to nested for loops
+const bubbleSort = (arr) => {
+  // use arr.length -1 so we don't waste time comparing the last value
+  for (let i = 0; i < arr.length - 1; i++) {
+    // don't bother comparing last value so arr.length -1 -i
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      // only do a swap if value on left greater than value on right
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+
+  return arr;
+};
+
+//
+//
+//
+// Version not using a variable but arrays
+const bubbleSort2 = (arr) => {
+  // use arr.length -1 so we don't waste time comparing the last value
+  for (let i = 0; i < arr.length - 1; i++) {
+    // don't bother comparing last value so arr.length -1 -i
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // get left and right to swap sides
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  return arr;
+};
+
+const my_array = [1, 2, 3, 10, 9, 8, -100, -200, -300];
+console.log(bubbleSort(my_array));
+console.log(bubbleSort2(my_array));
