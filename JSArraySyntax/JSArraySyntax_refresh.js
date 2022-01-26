5b06a2d53d64c26801ad0140fb8d48ef181796e8
mark <markpackham1@gmail.com>
2022-01-26 10:20:41 +0000

    add array syntax refresher
    https://www.youtube.com/watch?v=7W4pQQ20nJg

---
+++ JSArraySyntax/JSArraySyntax_refresh.js
@@ -0,0 +1,14 @@
+// JS Arrays revise (pretty much same as most languages) https://www.youtube.com/watch?v=7W4pQQ20nJg
+
+const a = ["A", "B", "C", "D"];
+console.log(a[0]);
+console.log(a.length);
+a.push(["E", "F"]);
+console.log(a);
+a.push([1, 2]);
+a.push([3]);
+console.log(a);
+a.pop([1]);
+console.log(a);
+console.log(a[0][0]);
+console.log(a[4][0]);
